af25af128b4d416feae7c0c7703ddebd5a323425
tanyapole <tanusha2406@gmail.com>
2021-06-09 09:30:56 +0000

    send imgs and return json

--- service.py
+++ service.py
@@ -1,10 +1,37 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
+import flask as F
+import torch
+from PIL import Image
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
+    # test any response
     print('[log] recieved query')
     return 'Hello, World!'
 
+@app.route("/im_size", methods=["POST"])
+def process_image():
+    # test sending images
+    file = F.request.files['image']
+    # Read the image via file.stream
+    img = Image.open(file.stream)
+
+    return jsonify({'msg': 'success', 'size': [img.width, img.height]})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print('[log] recieved POST predict')
+    print('!!!!! Files')
+    print(F.request.files)
+    print('!!!!! Data')
+    print(F.request.data)
+    return 'acne: 0.2'
+
+
+
 app.run(port=9000, threaded=False, host="127.0.0.1")
+
+
